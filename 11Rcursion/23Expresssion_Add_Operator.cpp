c5e4c4402165a1c5576631160bb7b8aed8d098f4
json77777 <json73747@gmail.com>
2026-04-26 16:44:28 +0000

    Refactor code structure for improved readability and maintainability

--- 11Rcursion/23Expresssion_Add_Operator.cpp
+++ 11Rcursion/23Expresssion_Add_Operator.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void addOperators(string &nums, int target, int i, int eval, int prev, vector<string> &ans, string temp) {
+void addOperators(string &nums, int target, int i, long long eval, long long prev, vector<string> &ans, string temp) {
 
     if(i==nums.size()) {
         if(target == 0) {
@@ -11,6 +11,27 @@ void addOperators(string &nums, int target, int i, int eval, int prev, vector<st
         return;
     }
 
+    for(int idx = i; idx < nums.length(); idx++) {
+        string part = nums.substr(i,idx-i+1);
+        if(part[0] == '0' && part.length()>1) {
+            break;
+        }
+
+        long long num = stoll(part);
+        if(i==0) {
+            addOperators(nums,target-num,idx+1,num,num,ans,part);
+        }
+        else {
+            // +
+            addOperators(nums,target-num, idx+1, eval+num, num, ans, temp+"+"+part);
+            // -
+            addOperators(nums,target+num, idx+1, eval-num, -num, ans, temp+"-"+part);
+            // -
+            addOperators(nums, target+prev-prev*num, idx+1, eval-prev+prev*num, prev*num, ans, temp+"*"+part);
+        }
+
+
+    }
     
 }
 
@@ -18,7 +39,7 @@ int main() {
     string nums = "123";
     int target = 6;
     vector<string> ans;
-    string temp;
+    string temp="";
     
     addOperators(nums,target,0,0,0,ans,temp);
     
