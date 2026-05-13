a2467a605ba0137f32491107a94fdc91261d4b5c
json77777 <json73747@gmail.com>
2026-05-13 13:14:04 +0530

    Add binary and decimal conversion functions; implement bit manipulation operations

--- 12Bit Manipulations/1basic.cpp
+++ 12Bit Manipulations/1basic.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// decimal to binary
 void binary(int n, string &ans) {
     if(!n) {
         ans.push_back('0');
@@ -15,9 +16,29 @@ void binary(int n, string &ans) {
     reverse(ans.begin(),ans.end());
 }
 
+
+// binary to decimal
+double decimal(string s) {
+    int ans=0;
+    int p=1;
+
+    // 101
+    for(int i=s.length()-1; i>=0; i--) {
+        if(s[i]=='1') {
+            ans+=(s[i]-'0')*p;
+        }
+        p*=2;
+    }
+    return (double)ans;
+}
+
+
 int main() {
     int n = 15;
     string ans = "";
     binary(n,ans);
-    cout << ans;
+    cout << ans << endl;
+
+    cout << decimal("1111") << endl;
+    
 }
---
+++ 12Bit Manipulations/2problems.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1
+void swap(int a, int b) {
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    cout << a << " " << b << endl;
+}
+
+// 2
+void checkBit(int n, int i) {
+    // n-> number, i -> position
+    if( n & (1 << i) ) cout << "yes";
+    else cout << "no";
+    cout << endl;
+}
+
+
+// 3
+void setBit(int n, int i) {
+    // n -> number i-> bit to set
+    cout << (n | (1 << i)) << endl;
+}
+
+
+// 4
+void resetBit(int n, int i) {
+    // n -> number i-> bit to set
+    cout << (n & ~(1 << i)) << endl;
+}
+
+
+// 5 toggle ith bit
+void toggleIthBit(int n, int i) {
+    cout << (n ^ (1 << i)) << endl;
+}
+
+
+// 6
+void clearRightMostSetbit(int n) {
+    cout << (n & n-1) << endl;
+}
+
+
+// 7
+// power of 2
+void checkPowOf2(int n) {
+    if(n & n-1) cout << "no";
+    else cout << "yes";
+    cout << endl;
+}
+
+
+// count setbits
+void countSetBits(int n) {
+    int c=0;
+    while(n) {
+        //  n & 1 -> odd check
+        c += n&1; // if it gives 1 that means remainder use the case of 13&1 gives 1 last bit
+        n = n>>1; // divide by 2
+    }
+    cout << c << endl;
+}
+
+void countSetBits_second(int n) {
+    int c=0;
+    while(n) {
+        n = n & (n-1);
+        c++;
+    }
+
+    cout << "Second way: " << c << endl;
+}
+
+int main() {
+    int a=5,b=6;
+    swap(a,b);
+    checkBit(13,2);
+    setBit(9,2);
+    resetBit(9,2);
+    toggleIthBit(13,2);
+    clearRightMostSetbit(12);
+    checkPowOf2(16);
+    countSetBits(15);
+    countSetBits_second(15);
+}
