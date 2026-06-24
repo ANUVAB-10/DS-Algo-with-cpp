22b08d2f08502378714b907ff23a35657c397a5d
json77777 <json73747@gmail.com>
2026-06-24 06:15:14 +0000

    Add implementation for binary tree construction and traversal methods

---
+++ 17Binary Tree/2Traversla.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+#include<optional>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* left=nullptr;
+    Node* right=nullptr;
+
+    Node(int x) {
+        this->val=x;
+    }
+
+    Node(int x, Node* l, Node* r) {
+        this->val=x;
+        this->left=l;
+        this->right=r;
+    }
+
+};
+
+Node* constructTree(const vector<optional<int>>& nums) {
+    if (nums.empty() || !nums[0].has_value())
+        return nullptr;
+
+    Node* root = new Node(*nums[0]);
+    queue<Node*> q;
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < nums.size()) {
+        Node* curr = q.front();
+        q.pop();
+
+        // left child
+        if (i < nums.size()) {
+            if (nums[i].has_value()) {
+                curr->left = new Node(*nums[i]);
+                q.push(curr->left);
+            }
+            ++i;
+        }
+
+        // right child
+        if (i < nums.size()) {
+            if (nums[i].has_value()) {
+                curr->right = new Node(*nums[i]);
+                q.push(curr->right);
+            }
+            ++i;
+        }
+    }
+
+    return root;
+}
+
+void preorder(Node* root, vector<int>&ans) {
+    if(!root) return;
+    ans.push_back(root->val);
+    preorder(root->left, ans);
+    preorder(root->right, ans);
+}
+
+void inorder(Node* root, vector<int>&ans) {
+    if(!root) return;
+    inorder(root->left, ans);
+    ans.push_back(root->val);
+    inorder(root->right, ans);
+}
+
+int main() {
+    // tree build
+    vector<optional<int>> nums = {1,2,3,4,5,nullopt,8,nullopt,nullopt,6,7,9};
+    Node* root = constructTree(nums);
+    
+    vector<int> ans;
+    // preorder(root, ans);
+    inorder(root, ans);
+
+    // print
+    cout << "pre-order traversal: ";
+    for(auto i: ans) {
+        cout << i << " ";
+    }cout << endl;
+}
